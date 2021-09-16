3598ee81b4427839169a2684a9b8aa504dc457aa
DaidotM <dadacorr@gmail.com>
2021-09-16 11:39:04 -0300

    Testing Ruler

--- script.js
+++ script.js
@@ -24,3 +24,32 @@ const doubleClick = (e) => {
     document.addEventListener('mousemove', onMouseMove);
 }
 document.addEventListener('dblclick', doubleClick);
+
+//Test ruler
+function drawOnCanvas(y) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(20, y);
+    ctx.stroke();
+}
+function drawRuler() {
+    var ppm = 8.602150744349842,
+        count = Math.floor(c.height / ppm);
+
+    for (var i = 0; i < count; i++) {
+        var topPos = (5 + i * ppm);
+        drawOnCanvas(Math.floor(topPos) + .5);
+    };
+
+}
+function resizeCanvas() {
+    c.width = document.body.clientWidth - 5,
+        c.height = document.body.clientHeight - 5;
+}
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.lineWidth = 1;
+
+resizeCanvas();
+drawRuler();
